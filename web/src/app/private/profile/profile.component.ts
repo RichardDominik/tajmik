22168153ed0a69db744244c4996bfd6eb9e23e9d
Richard Dominik <riso.dominik@gmail.com>
2016-12-27 14:04:10 +0100

    Dashboard menu

--- web/src/app/private/profile/profile.component.ts
+++ web/src/app/private/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Subscription } from 'rxjs/Rx';
+import { JwtHelper } from 'angular2-jwt';
+
+import { AuthService } from './../../auth/auth.service';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+	isLogged: boolean;
 
-  constructor() { }
+	jwtHelper: JwtHelper = new JwtHelper();
+	token: string;
+	tokenDecoded: Object;
+	tokenEmail: string;
 
-  ngOnInit() {
-  }
+  	constructor(private authService: AuthService) {}
 
+  	ngOnInit() {
+  		this.isLogged = this.authService.isAuthenticate();
+  		if(this.isLogged){
+  			this.token = localStorage.getItem('token');
+  			this.tokenDecoded = this.jwtHelper.decodeToken(this.token);
+  			this.tokenEmail = this.tokenDecoded['email'];
+  		}
+  	}
 }
