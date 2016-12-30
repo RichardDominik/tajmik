9c0f527df46cbcb410d23990d6d0ce1fe5f4f5f3
Richard Dominik <riso.dominik@gmail.com>
2016-12-30 15:09:13 +0100

    Task manager frontend logic

--- web/src/app/private/task-manager/task-manager.component.ts
+++ web/src/app/private/task-manager/task-manager.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { AppComponent } from '../../app.component';
+
+import { Task } from './task';
+import { TaskDataService } from './task-data.service';
 
 @Component({
   selector: 'app-task-manager',
   templateUrl: './task-manager.component.html',
-  styleUrls: ['./task-manager.component.css']
+  styleUrls: ['./task-manager.component.css'],
+  providers: [TaskDataService]
 })
-export class TaskManagerComponent implements OnInit {
+
+export class TaskManagerComponent {
 
 	isLogged: boolean;
+	newTask: Task = new Task();
 
-  	constructor(private appComponent: AppComponent) {
+  	constructor(private appComponent: AppComponent, private taskdataService: TaskDataService) {
   		this.isLogged = this.appComponent.isLogged;
    	}
 
-  	ngOnInit() {
-  	}
+   	addTask() {
+   		this.taskdataService.addTask(this.newTask);
+   		this.newTask = new Task();
+   	}
+
+   	toggleTaskComplete(task){
+   		this.taskdataService.toggleTaskComplete(task);
+   	}
+
+   	removeTask(task){
+   		this.taskdataService.deleteTaskById(task.id);
+   	}
+
+   	get tasks(){
+   		return this.taskdataService.getAllTasks();
+   	}
+
 }
