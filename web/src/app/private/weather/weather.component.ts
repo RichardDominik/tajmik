fe2f60741b475a99bd603cbfdb4535673345a2ea
Richard Dominik <riso.dominik@gmail.com>
2017-01-04 16:06:12 +0100

    Weather component show data in template

--- web/src/app/private/weather/weather.component.ts
+++ web/src/app/private/weather/weather.component.ts
@@ -11,16 +11,28 @@ import { WeatherService } from './weather.service';
 export class WeatherComponent implements OnInit {
 
 	isLogged: boolean;
+  city: string;
+  location: any;
+  forecast: any;
+  allForecast: any;
+
 	
   	constructor(private appComponent: AppComponent, private weatherService: WeatherService) {
   		this.isLogged = this.appComponent.isLogged;
       this.weatherService = weatherService;
+      this.city = 'Vranov nad Toplou';
+      this.location;
+      this.forecast;
+      this.allForecast;
    	}
 
   	ngOnInit() {
-      this.weatherService.getWeather()
-          .subscribe(weather => {
-            console.log(weather)
+      this.weatherService.getWeather(this.city)
+          .subscribe(forecast => {
+            this.location = forecast.city;
+            this.forecast = forecast.list[0];
+            this.allForecast = forecast.list;
           });
   	}
 }
+
--- web/src/app/private/weather/weather.service.ts
+++ web/src/app/private/weather/weather.service.ts
@@ -7,16 +7,17 @@ import 'rxjs/Rx';
 export class WeatherService {
 	apiKey: string;
 	weatherUrl: string;
+  apiUser: string;
 
  	constructor(@Inject(Http) private http: Http) {
   		this.http = http;
   		this.apiKey = '24d021f2f41006ac5f062da3896dfd8c';
-  		this.weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=json&appid='+this.apiKey+''
-  		console.log('Ahojda chcem fungovat!');
+  		this.weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?'
+      this.apiUser = ',us&mode=json&appid='+this.apiKey+'';
   	}
 
-  	getWeather() {
-  		return this.http.get(this.weatherUrl)
+  	getWeather(city) {
+  		return this.http.get(this.weatherUrl + 'q= '+ city + this.apiUser)
   				.map(res => res.json());
   	}
 }
