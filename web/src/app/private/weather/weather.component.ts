790cdd3a3e81f7f968a81c65b7d8a2d8fe2b62b6
Richard Dominik <riso.dominik@gmail.com>
2017-01-27 18:45:36 +0100

    city in WeatherComponent from database

--- web/src/app/private/weather/weather.component.ts
+++ web/src/app/private/weather/weather.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { WeatherService } from './weather.service';
+import { ProfileService } from '../profile/profile.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css'],
-  providers: [WeatherService]
+  providers: [WeatherService, ProfileService]
 })
+
 export class WeatherComponent implements OnInit {
 
 	isLogged: boolean;
@@ -15,24 +17,30 @@ export class WeatherComponent implements OnInit {
   location: any;
   forecast: any;
   allForecast: any;
+  res: any;
 
-	
-  	constructor(private appComponent: AppComponent, private weatherService: WeatherService) {
+  	constructor(private appComponent: AppComponent, private weatherService: WeatherService, private profileService: ProfileService) {
   		this.isLogged = this.appComponent.isLogged;
       this.weatherService = weatherService;
-      this.city = 'Kosice';
       this.location;
       this.forecast;
       this.allForecast;
    	}
 
   	ngOnInit() {
-      this.weatherService.getWeather(this.city)
-          .subscribe(forecast => {
-            this.location = forecast.city;
-            this.forecast = forecast.list[0];
-            this.allForecast = forecast.list;
-          });
+      this.profileService.getProfile()
+        .subscribe(res => {
+             this.res = res[0];
+             if(res[0] !== undefined){
+                this.city = res[0].city;
+                this.weatherService.getWeather(this.city)
+                  .subscribe(forecast => {
+                    this.location = forecast.city;
+                    this.forecast = forecast.list[0];
+                    this.allForecast = forecast.list;
+                });
+             }
+        }); 
   	}
 }
 
