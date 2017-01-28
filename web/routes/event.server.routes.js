088bcde32e606b9da6386c9dd2a80befddca2ee5
Richard Dominik <riso.dominik@gmail.com>
2017-01-28 13:05:59 +0100

    API's for get length, completed, incompleted tasks and events

--- web/routes/event.server.routes.js
+++ web/routes/event.server.routes.js
@@ -2,6 +2,7 @@ var Event = require('../models/event.js');
 
 module.exports = function(app) {
 
+	// post events to DB
 	app.post('/api/events', function(req, res){
 		var event = new Event(req.body);
 
@@ -17,6 +18,7 @@ module.exports = function(app) {
 		})
 	});
 
+	//get events from DB
 	app.get('/api/events', function(req, res){
 		Event.find({creator: req.headers.authorization}).sort('-created').exec(function(err, events){
 			if(err){
@@ -28,6 +30,48 @@ module.exports = function(app) {
 				res.json(events);
 			}
 		});
+	});
+
+	//get length of events
+	app.get('/api/events/length', function(req, res){
+		Event.find({creator: req.headers.authorization}).count()
+			.exec(function(err, events){
+				if(err){
+					return res.status(400).send({
+						message: 'error'
+					});
+				} else {
+					res.json(events);
+				}
+			});
+	});
+
+	//get length of incompleted events
+	app.get('/api/events/incompleted', function(req, res){
+		Event.find({creator: req.headers.authorization, completed: false}).count()
+			.exec(function(err, events){
+				if(err){
+					return res.status(400).send({
+						message: 'error'
+					});
+				} else {
+					res.json(events);
+				}
+			});
+	});
+
+	//get length of completed events
+	app.get('/api/events/completed', function(req, res){
+		Event.find({creator: req.headers.authorization, completed: true}).count()
+			.exec(function(err, events){
+				if(err){
+					return res.status(400).send({
+						message: 'error'
+					});
+				} else {
+					res.json(events);
+				}
+			});
 	});
 
 	app.get('/api/events/:eventID', function(req, res){
--- web/routes/tasks.server.routes.js
+++ web/routes/tasks.server.routes.js
@@ -1,6 +1,7 @@
 var Task = require('../models/task.js');
 
 module.exports = function(app) {
+	// post tasks to DB
 	app.post('/api/tasks', function(req, res){
 		var task = new Task(req.body);
 
@@ -16,6 +17,7 @@ module.exports = function(app) {
 		})
 	});
 
+	//get tasks from DB
 	app.get('/api/tasks', function(req, res){
 		Task.find({creator: req.headers.authorization}).exec(function(err, tasks){
 			if(err){
@@ -27,6 +29,48 @@ module.exports = function(app) {
 				res.json(tasks);
 			}
 		});
+	});
+
+	//get length of tasks
+	app.get('/api/tasks/length', function(req, res){
+		Task.find({creator: req.headers.authorization}).count()
+			.exec(function(err, tasks){
+				if(err){
+					return res.status(400).send({
+						message: 'error'
+					});
+				} else {
+					res.json(tasks);
+				}
+			});
+	});
+
+	//get length of incompleted tasks
+	app.get('/api/tasks/incompleted', function(req, res){
+		Task.find({creator: req.headers.authorization, completed: false}).count()
+			.exec(function(err, tasks){
+				if(err){
+					return res.status(400).send({
+						message: 'error'
+					});
+				} else {
+					res.json(tasks);
+				}
+			});
+	});
+
+	//get length of completed tasks
+	app.get('/api/tasks/completed', function(req, res){
+		Task.find({creator: req.headers.authorization, completed: true}).count()
+			.exec(function(err, tasks){
+				if(err){
+					return res.status(400).send({
+						message: 'error'
+					});
+				} else {
+					res.json(tasks);
+				}
+			});
 	});
 
 };
