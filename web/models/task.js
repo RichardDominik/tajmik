cfc73e8cf32858bcb45bc109a46a4c31fff84dd3
Richard Dominik <riso.dominik@gmail.com>
2017-01-28 12:39:51 +0100

    Task schema

--- web/models/task.js
+++ web/models/task.js
@@ -2,9 +2,7 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let schema = new Schema({
-	created: {type: Date, default: Date.now},
-	title: {type: String, trim: true, required: "Please enter a Event"},
-	comment: {type: String},
+	title: {type: String, trim: true, required: "Please enter a Task"},
 	creator: {type: String},
 	completed: {type: Boolean, default: false}
 });
